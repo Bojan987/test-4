264366bd640fe43db009ca3e915cd3c7238dc20b
bojan987 <bojanmaj@hotmail.com>
2020-09-14 10:00:58 +0200

    pocetak

---
+++ test4/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = ()=>{
+  return <p>Hello</p>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
